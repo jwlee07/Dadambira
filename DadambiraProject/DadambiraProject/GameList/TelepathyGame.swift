556b153bb72260b45b8a8fed78c90c934dace365
jwlee07 <apzk21413@gmail.com>
2020-06-23 17:15:14 +0900

    proj: Telelpacy_v1

--- DadambiraProject/DadambiraProject/GameList/TelepathyGame.swift
+++ DadambiraProject/DadambiraProject/GameList/TelepathyGame.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class TelepathyGameViewController: UIViewController {
   
+  let personNumberSetupButton: UIButton = {
+    let button = UIButton()
+    return button
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    title = "텔레파시 으잉!"
+    setupNavigationBar()
     
   }
+  
+  func setupNavigationBar() {
+    let leftDismissButton = UIBarButtonItem (image: UIImage(systemName: "arrowshape.turn.up.left.fill"), style: .plain, target: self, action: #selector(didTapDismissButton))
+    leftDismissButton.tintColor = UIColor(red: 66/255, green: 72/255, blue: 166/255, alpha: 1)
+    
+    navigationItem.leftBarButtonItem = leftDismissButton
+    navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+    navigationController?.navigationBar.shadowImage = UIImage()
+    navigationController?.navigationBar.backgroundColor = UIColor.clear
+  }
+  
+  @objc func didTapDismissButton(_ sender: UIBarButtonItem) {
+    
+  }
+  
 }
