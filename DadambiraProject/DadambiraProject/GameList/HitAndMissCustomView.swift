8af77e009ee7944567cd247bc92b373348a45716
jwlee07 <apzk21413@gmail.com>
2020-06-26 02:56:38 +0900

    200626 jwlee07

--- DadambiraProject/DadambiraProject/GameList/HitAndMissCustomView.swift
+++ DadambiraProject/DadambiraProject/GameList/HitAndMissCustomView.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class HitAndMissCustomView: UIViewController {
   // MARK: - Property
-  
-  
   let alertImageView = UIImageView()
   var alertImage = UIImage()
   
@@ -21,10 +19,8 @@ class HitAndMissCustomView: UIViewController {
   }
   // MARK: - setup Layout
   func setupLayout() {
-//   alertImageView.frame = CGRect(x:0, y:0, width:(alertImage!.size.width),height:(alertImage!.size.height))
     alertImageView.frame = CGRect(x:0, y:-5, width: 75 ,height: 75)
     self.view.addSubview(alertImageView)
     self.preferredContentSize = CGSize(width:75,height:85)
-//    self.preferredContentSize = CGSize(width:(alertImage!.size.width),height:(alertImage!.size.height)+10)
   }
 }
